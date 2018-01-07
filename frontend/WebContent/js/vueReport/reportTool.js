1bc7cfedaa64071a9b7bbb8d69344a1aa8ac3573
qisibajie <qisibajie@163.com>
2018-01-07 15:18:54 +0800

    add v-for sample

--- frontend/WebContent/js/vueReport/reportTool.js
+++ frontend/WebContent/js/vueReport/reportTool.js
@@ -4,10 +4,27 @@ var app = new Vue({
         message: 'Hello Vue!'
     }
 });
-var app2= new Vue({
+var app2 = new Vue({
     el: '#app-2',
-    data:{
-        message: '页面加载于'+new Date().toLocaleDateString()
+    data: {
+        message: '页面加载于' + new Date().toLocaleDateString()
     }
 });
 
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+});
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            {text: '看书'},
+            {text: '写博客'},
+            {text: '提高影响力'}
+        ]
+    }
+});
