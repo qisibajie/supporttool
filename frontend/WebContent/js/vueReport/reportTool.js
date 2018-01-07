479c8c93d59e4ee0030ae9ed4ff0717be1c31085
qisibajie <qisibajie@163.com>
2018-01-07 15:29:33 +0800

    add v-on:click event sample

--- frontend/WebContent/js/vueReport/reportTool.js
+++ frontend/WebContent/js/vueReport/reportTool.js
@@ -28,3 +28,15 @@ var app4 = new Vue({
         ]
     }
 });
+
+var app5 = new Vue({
+   el:'#app-5',
+   data:{
+       message: 'Hello , Vue'
+   },
+    methods:{
+       reverseMessage: function () {
+           this.message = this.message.split(' ').reverse().join(' ');
+       }
+    }
+});
